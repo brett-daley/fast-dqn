bbf59d039110c692a92d87e50845eb58fec29900
Brett Daley <brett-daley@users.noreply.github.com>
2021-07-19 18:28:33 -0400

    Faster minibatch coalescing

--- fast_dqn/deep_q_network.py
+++ fast_dqn/deep_q_network.py
@@ -40,29 +40,24 @@ class DeepQNetwork:
     def train(self, states, actions, rewards, next_states, dones, split=1):
         assert states.shape[0] % split == 0
         batch_size = states.shape[0] // split
-        for i in range(split):
-            s = slice(i * batch_size, (i + 1) * batch_size)
-            Q = self._train(states[s], actions[s], rewards[s], next_states[s], dones[s])
-            if i == 0:
-                next_action = tf.argmax(Q[0])
-        return next_action
 
-    def _train(self, states, actions, rewards, next_states, dones):
         next_Q = self._predict_target(next_states)
-        max_Q = tf.reduce_max(next_Q, axis=1)
+        targets = rewards + self.discount * (1.0 - dones) * tf.reduce_max(next_Q, axis=1)
+        mask = tf.one_hot(actions, depth=next_Q.shape[1])
 
-        with tf.GradientTape() as tape:
-            Q = self.predict(states)
-            mask = tf.one_hot(actions, depth=Q.shape[1])
-            Q = tf.reduce_sum(mask * Q, axis=1)
+        for i in range(split):
+            sl = slice(i * batch_size, (i + 1) * batch_size)
 
-            targets = rewards + self.discount * (1.0 - dones) * max_Q
-            loss = tf.keras.losses.MSE(targets, Q)
+            with tf.GradientTape() as tape:
+                Q = self.predict(states[sl])
+                Q = tf.reduce_sum(mask[sl] * Q, axis=1)
+                loss = tf.keras.losses.MSE(targets[sl], Q)
 
-        gradients = tape.gradient(loss, self._main_vars)
-        self.optimizer.apply_gradients(zip(gradients, self._main_vars))
+            gradients = tape.gradient(loss, self._main_vars)
+            self.optimizer.apply_gradients(zip(gradients, self._main_vars))
 
-        return next_Q
+        next_action = tf.argmax(next_Q[0])
+        return next_action
 
     @tf.function
     def update_target_net(self):
